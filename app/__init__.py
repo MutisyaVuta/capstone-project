b17e1ad75d980830c217391af7f317491b8348a5
louis mulaa <louismulaa06@gmail.com>
2024-08-02 10:19:28 +0300

    flask application set up

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import Config 
+
+from flask_bcrypt import Bcrypt
+
+from flask_jwt_extended import JWTManager
+
+from flask_cors import CORS 
+
+db=SQLAlchemy()
+migrate=Migrate()
+bcrypt= Bcrypt()
+jwt=JWTManager()
+
+def create_app():#Sets up and configures the Flask app.
+    app=Flask(__name__)
+    app.config.from_object(Config)  # Load config from the Config class
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app,db)
+    from .models import User,Book,Loan
+    CORS(app) #Enables cross-origin requests from any domain.
+
+    #import blue prints 
+    #register blue prints 
+    return app
