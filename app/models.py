75d9a62db5a980ea5130df0917d64283f03ee056
louis mulaa <louismulaa06@gmail.com>
2024-08-05 15:57:53 +0300

    finished my bit

--- app/models.py
+++ app/models.py
@@ -9,12 +9,19 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    role = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable=True)
     admn_no = db.Column(db.String(20), unique=True, nullable=False)
     loans = db.relationship('Loan', backref='user', lazy=True)#user can have multiple loans 
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+    
+    def details(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'admn_no': self.admn_no  # Include admn_no in the dictionary
+        }
+    #def __repr__(self):
+        #return f"User('{self.username}', '{self.email}')"
 
 class Book(db.Model):
     __tablename__='book'
